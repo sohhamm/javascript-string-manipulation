859b928b018f39ac0d583f7b8e411ced3588a0b4
Soham Sarkar <soham.ss1@gmail.com>
2021-02-06 09:29:37 +0530

    resolved the bug

--- src/info.js
+++ src/info.js
@@ -2,28 +2,21 @@ const usage = 'Usage: crlf-convert [CR|LF|CRLF]';
 
 const help = String.raw`${usage}
 
+Converts text between newline (\n), carriage return (\r), and combined
+(\r\n) line endings.
 
-           Converts text between newline (\n), carriage return (\r), and combined
+Options:
 
-            (\r\n) line endings.
+[CR|LF|CRLF]: Desired line ending
 
+    CR is a carriage return character \r (rare)
+    LF is a line feed character \n (common on Unix and macOS)
+    CRLF is a combined ending \r\n (common on Windows)
 
-            Options:
+Example:
 
-
-            [CR|LF|CRLF]: Desired line ending
-
-
-                CR is a carriage return character \r (rare)
-
-                LF is a line feed character \n (common on Unix and macOS)
-
-                CRLF is a combined ending \r\n (common on Windows)
-
-            Example:
-
-
-                cat in.txt | crlf-convert LF > out.txt`;
+    cat in.txt | crlf-convert LF > out.txt
+`;
 
 module.exports = {
   help,
